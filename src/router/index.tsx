a574e65120430064c5ea4a0196b1da64072a4b12
MingChuan Jiang <trueshinygold@gmail.com>
2020-02-10 18:42:57 +0800

    add forgotpwd logic, fix ui

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,6 +5,7 @@ import loadable from "@loadable/component";
 const Layout = loadable(() => import("../common/layout"));
 const Login = loadable(() => import("../pages/login"));
 const Register = loadable(() => import("../pages/register"));
+const ForgotPwd = loadable(() => import("../pages/forgotpwd"));
 const ArticleList = loadable(() => import("../pages/article-list"));
 const AddArticle = loadable(() => import("../pages/article-add"));
 const ArticleClassify = loadable(() => import("../pages/article-classify"));
@@ -47,6 +48,10 @@ export const routes = [
   {
     path: "/register",
     component: Register
+  },
+  {
+    path: "/forgotpwd",
+    component: ForgotPwd
   },
   {
     path: "/",
